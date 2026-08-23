ae1f6c59271e7b681a04dcb3be37383aa095e379
Shreya Singh <shreya02082007@gmail.com>
2026-08-23 19:59:58 +0530

    Add Top K Frequent Elements solution

---
+++ Arrays/TopKFrequentElements.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class TopKFrequentElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> map.get(a) - map.get(b)
+        );
+
+        for (int num : map.keySet()) {
+            pq.add(num);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = pq.poll();
+        }
+
+        return ans;
+    }
+}
