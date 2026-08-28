b97c82bacfb5de632279e868ed10f3a83b6d6612
Shreya Singh <shreya02082007@gmail.com>
2026-08-28 22:41:18 +0530

    Add Word Pattern solution

---
+++ Strings/WordPattern.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class WordPattern {
+
+    public boolean wordPattern(String pattern, String s) {
+
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> reverseMap = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map.containsKey(ch) && !map.get(ch).equals(word)) {
+                return false;
+            }
+
+            if (reverseMap.containsKey(word) && reverseMap.get(word) != ch) {
+                return false;
+            }
+
+            map.put(ch, word);
+            reverseMap.put(word, ch);
+        }
+
+        return true;
+    }
+}
