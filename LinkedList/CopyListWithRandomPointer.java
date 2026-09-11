f74e887b186da4d6b0e34b6708c29537980b1e21
Shreya Singh <shreya02082007@gmail.com>
2026-09-11 20:45:19 +0530

    Add Copy List with Random Pointer solution

---
+++ LinkedList/CopyListWithRandomPointer.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class CopyListWithRandomPointer {
+
+    public Node copyRandomList(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        Node current = head;
+
+        while (current != null) {
+            map.put(current, new Node(current.val));
+            current = current.next;
+        }
+
+        current = head;
+
+        while (current != null) {
+            map.get(current).next = map.get(current.next);
+            map.get(current).random = map.get(current.random);
+            current = current.next;
+        }
+
+        return map.get(head);
+    }
+}
