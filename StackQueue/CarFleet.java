f59027490e49efa03c4ac530565817e229869b85
Shreya Singh <shreya02082007@gmail.com>
2026-09-17 21:08:54 +0530

    Add Car Fleet solution

---
+++ StackQueue/CarFleet.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class CarFleet {
+
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        int n = position.length;
+        double[][] cars = new double[n][2];
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double) (target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (cars[i][1] > lastTime) {
+                fleets++;
+                lastTime = cars[i][1];
+            }
+        }
+
+        return fleets;
+    }
+}
