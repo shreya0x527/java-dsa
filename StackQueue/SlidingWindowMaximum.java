0f57d09077eef49d88acf7523a05f8528e436e48
Shreya Singh <shreya02082007@gmail.com>
2026-09-18 22:01:12 +0530

    Add Sliding Window Maximum solution

---
+++ StackQueue/SlidingWindowMaximum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class SlidingWindowMaximum {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() &&
+                   nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                ans[index++] = nums[deque.peekFirst()];
+            }
+        }
+
+        return ans;
+    }
+}
