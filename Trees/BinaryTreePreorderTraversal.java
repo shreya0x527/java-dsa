1dd03ea1b735522a54f0c7c75241711e47692831
Shreya Singh <shreya02082007@gmail.com>
2026-09-24 22:06:04 +0530

    Add Binary Tree Preorder Traversal solution

---
+++ Trees/BinaryTreePreorderTraversal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return ans;
+    }
+}
